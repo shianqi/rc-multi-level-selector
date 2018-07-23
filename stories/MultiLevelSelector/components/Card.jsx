f75ca9bd231a30e6c6b8bc8a988457c318fa3359
shianqi <shianqi@aliyun.com>
2018-07-23 11:06:56 +0800

    Modify some styles

--- stories/MultiLevelSelector/components/Card.jsx
+++ stories/MultiLevelSelector/components/Card.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const Card = styled.div`
-  padding-bottom: 20px;
+  padding-bottom: 30px;
 `
 
 export default Card
--- stories/MultiLevelSelector/components/Container.jsx
+++ stories/MultiLevelSelector/components/Container.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const Container = styled.div`
   max-width: 700px;
   margin: 100px auto;
+  padding: 0 20px;
 `
 
 export default Container
