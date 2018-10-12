e3f8ac2547d4b3504b5ba817fb88ec8f391ee674
shianqi <mail@shianqi.com>
2018-10-12 22:04:13 +0800

    onChange will trigger as the option changes

--- src/components/MultiLevelSelector/index.jsx
+++ src/components/MultiLevelSelector/index.jsx
@@ -14,6 +14,7 @@ class MultiLevelSelector extends React.PureComponent {
 
     const { options } = props
 
+    this.oldOptions = options
     this.state = {
       value: this.initValue(options),
     }
@@ -24,10 +25,36 @@ class MultiLevelSelector extends React.PureComponent {
 
   componentDidMount () {
     const { value } = this.state
-    const { onDefaultValue } = this.props
+    const { onDefaultValue, options } = this.props
 
-    const selectedObject = this.getSelectedObject(value)
+    const selectedObject = this.getSelectedObject(value, options)
     onDefaultValue && onDefaultValue(selectedObject)
+  }
+
+  arrayIsIdentical (array1, array2) {
+    if (array1 == null || array2 == null || array1.length !== array2.length) {
+      return false
+    }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i].id !== array2[i].id) {
+        return false
+      }
+    }
+    return true
+  }
+
+  componentDidUpdate () {
+    const { value } = this.state
+    const { onChange, options } = this.props
+
+    const newValue = this.getSelectedObject(value, options)
+    const oldValue = this.getSelectedObject(this.oldValue, this.oldOptions || options)
+
+    if (!this.arrayIsIdentical(newValue, oldValue)) {
+      onChange && onChange(newValue)
+    }
+    this.oldValue = value
+    this.oldOptions = options
   }
 
   initValue (options) {
@@ -55,23 +82,28 @@ class MultiLevelSelector extends React.PureComponent {
     })
   }
 
-  getSelectedObject (value) {
-    const { options } = this.props
+  getSelectedObject (value, options) {
+    if (value == null) {
+      return null
+    }
 
     let opt = options
     const data = []
 
-    value.forEach((i) => {
+    for (let i of value) {
+      if (!opt || !opt[i]) {
+        return data
+      }
       const { item, ...dataItem } = opt[i]
       data.push(dataItem)
       opt = item
-    })
+    }
 
     return data
   }
 
   handleOnChange (id, index, subOptions) {
-    const { onChange, subOptionKey } = this.props
+    const { onChange, subOptionKey, options } = this.props
     const value = this.getValueIndexs() || this.state.value
 
     const head = value.slice(0, index)
@@ -85,7 +117,7 @@ class MultiLevelSelector extends React.PureComponent {
     this.setState({
       value: res,
     })
-    onChange && onChange(this.getSelectedObject(res))
+    onChange && onChange(this.getSelectedObject(res, options))
   }
 
   renderSelector (index, options) {
@@ -99,7 +131,7 @@ class MultiLevelSelector extends React.PureComponent {
       const value = this.getValueIndexs() || this.state.value
 
       const selectIndex = value[index]
-      const itemSelected = options[selectIndex]
+      const itemSelected = options[selectIndex] || {}
 
       return (
         <Fragment>
